2c23dc5612bbc45ed4593f168d2a4022f64a240f
DavidIC43 <blak_storm67@hotmail.com>
2021-04-14 11:21:32 -0500

    mi primer commit

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyB97DJ5cIN6jSt-EFZmT1fau_acLPLrn2I",
+    authDomain: "bddavidic43.firebaseapp.com",
+    projectId: "bddavidic43",
+    storageBucket: "bddavidic43.appspot.com",
+    messagingSenderId: "556439627302",
+    appId: "1:556439627302:web:40ab4eff3af11419dc68e7",
+    measurementId: "G-WGGSM4FX91"
+});
